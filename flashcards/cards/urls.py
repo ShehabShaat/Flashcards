9b196485f319861a83df39932349a115003b757a
shehabSh <shehab.e.sh@gmail.com>
2023-02-05 00:28:40 +0200

    new update

---
+++ flashcards/cards/urls.py
@@ -0,0 +1,42 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    
+    path(
+        "",
+        TemplateView.as_view(template_name="cards/base.html"),
+        name="home"
+    ),
+]
+
+
+from django.urls import path
+
+# Removed: from django.views.generic import TemplateView
+
+from . import views
+
+urlpatterns = [
+
+    path(
+        "",
+        views.CardListView.as_view(),
+        name="card-list"
+    ),
+       path(
+        "new",
+        views.CardCreateView.as_view(),
+        name="card-create"
+    ),
+        path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
+    ),
+          path(
+        "box/<int:box_num>",
+        views.BoxView.as_view(),
+        name="box"),
+
+]
